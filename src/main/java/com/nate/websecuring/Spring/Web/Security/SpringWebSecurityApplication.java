87d28bd67e2aeea486c0561c57df32d999315e3b
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-03 00:27:58 +0000

    Initial Commit

---
+++ src/main/java/com/nate/websecuring/Spring/Web/Security/SpringWebSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.nate.websecuring.Spring.Web.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSecurityApplication.class, args);
+	}
+
+}
